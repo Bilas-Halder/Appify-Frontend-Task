2a3f939a33b8e5c99654155983e08a5e2bd1a3fa
Bilas-Halder <bilas8642@gmail.com>
2022-09-04 01:14:34 +0600

    Left Side Done

---
+++ src/Components/LeftSection/LeftSection.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { FiSave } from "react-icons/fi";
+import playStore from "../../Images/download play store.png";
+import appStore from "../../Images/download app store.png";
+import "./LeftSection.css";
+import Button from "react-bootstrap/Button";
+
+const componentName = (props) => {
+  return (
+    <>
+      <div className="left-section explore">
+        <h4>Explore</h4>
+        <div className="explore-rows">
+          <div className="explore-item">
+            <FiSave /> <span> Saved Posts</span>
+          </div>
+          <div className="explore-item">
+            <FiSave /> <span> People</span>
+          </div>
+          <div className="explore-item">
+            <FiSave /> <span> Groups</span>
+          </div>
+          <div className="explore-item">
+            <FiSave /> <span> Pages</span>
+          </div>
+          <div className="explore-item">
+            <FiSave /> <span> Events</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="left-section download-our-app">
+        <h4>Download App</h4>
+        <div className="d-flex justify-content-between mt-3">
+          <div className="store-img app-store">
+            <img src={appStore} alt="" />
+          </div>
+          <div className="store-img play-store">
+            <img src={playStore} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="left-section suggested-people">
+        <h4>Suggested People</h4>
+        <div className="d-flex justify-content-between align-items-center mt-4">
+          <div className="peoples-img">
+            <img src={appStore} alt="" />
+          </div>
+          <div className="peoples-name">Sadek Hossian</div>
+
+          <Button variant="outline-secondary">Add+</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default componentName;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Header from "../../Components/Header/Header";
+import Explore from "../../Components/LeftSection/LeftSection";
 import "./HomePage.css";
 
 const componentName = (props) => {
   return (
-    <>
+    <div style={{ backgroundColor: "#e9ebee" }}>
       <Header />
       <Container>
         <div className="grid-system-home">
-          <div className="grid-column-home home-left">123</div>
+          <div className="grid-column-home home-left">
+            <Explore />
+          </div>
           <div className="grid-column-home home-mid">456</div>
           <div className="grid-column-home home-right">789</div>
         </div>
       </Container>
-    </>
+    </div>
   );
 };
 
